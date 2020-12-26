f5f8ecb67ccf2adc9825c5d2563d3dea03854921
Truong Thanh Thanh <truongthanh1303@MacBook-Pro.local>
2020-12-26 11:05:01 +0700

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,48 @@
+import random
+import string
+from words import words
+
+
+def generate_word():
+    word = random.choice(words)
+
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = generate_word()
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_words = set()
+
+    while len(word_letters) > 0:
+        print('Used words ', ' '.join(used_words))
+        words_list = []
+        # letter if letter in used_words else '-' for letter in word
+        for letter in word:
+            if letter in used_words:
+                words_list.append(letter)
+            else:
+                words_list.append('-')
+
+        print('Current words: ', ' '.join(words_list))
+
+        user_letter = input('Guess a letter ').upper()
+        if user_letter in alphabet - used_words:
+            used_words.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_words:
+            print('You have used this word already. Try again')
+
+        else:
+            print('You put an invalid word. Try again')
+
+    print('Yeah, you guessed this word: ', word)
+
+
+hangman()
